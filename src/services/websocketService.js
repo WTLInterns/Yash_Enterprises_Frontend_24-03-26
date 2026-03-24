20166a9b64a595da244305d435d57c2ce8fb712f
WTLInterns <wtlinterns2@gmail.com>
2026-03-24 16:03:08 +0530

    apireplace:

--- src/services/websocketService.js
+++ src/services/websocketService.js
@@ -49,7 +49,7 @@ class WebSocketService {
 
     return new Promise((resolve, reject) => {
       try {
-        const socket = new SockJS('http://localhost:8080/ws');
+        const socket = new SockJS('https://api.yashrajent.com/ws');
         this.client = new Client({
           webSocketFactory: () => socket,
           connectHeaders: {},
